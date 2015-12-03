663f5f616ad180855a97fe9b189d2934999e9245
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-02 20:22:56 -0500

    PauseMenu fully implemented

--- FinalProject/Assets/Scripts/MainMenu/PauseMenu.cs
+++ FinalProject/Assets/Scripts/MainMenu/PauseMenu.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
     public GameObject pauseMenu;
     public GameObject gameCamera;
+    public Button currentButton;
+
+    //public Button[] pauseMenuButtons;
+    //float transitionDelayTime = .7f;
+    //float transitionDelayTimer = 0;
     Transform player;
 
     void Start()
@@ -23,6 +29,10 @@ public class PauseMenu : MonoBehaviour {
     void setPauseScreenActive()
     {
         pauseMenu.SetActive(!pauseMenu.activeSelf);
+        if (pauseMenu.activeSelf)
+        {
+            currentButton.Select();
+        }
         player.GetComponent<Animator>().enabled = !pauseMenu.activeSelf;
         player.GetComponent<WalkMechanics>().setUpdateEnabled(!pauseMenu.activeSelf);
         player.GetComponent<PlayerAnimation>().enabled = !pauseMenu.activeSelf;
