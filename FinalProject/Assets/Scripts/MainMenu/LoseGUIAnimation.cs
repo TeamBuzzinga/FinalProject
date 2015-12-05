003e5a0bfc7d9c21d985bc72bc8e7a62c82fc70a
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-05 18:12:03 -0500

    Lose condition added

---
+++ FinalProject/Assets/Scripts/MainMenu/LoseGUIAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseGUIAnimation : MonoBehaviour {
+    Animator anim;
+    PlayerStats playerStats;
+    bool playerLost;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+    }
+
+    void Update()
+    {
+        if (playerStats.strikes > 3 && !playerLost)
+        {
+            anim.SetTrigger("LoseGame");
+        }
+    }
+}
