775a011abfa3e4de4eda996db941c6fe45130802
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-05 11:46:24 -0500

    Some updates to win condition

---
+++ FinalProject/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinCondition : MonoBehaviour {
+    public int checkKeyCollected;
+    public int checkItemsCollected;
+    public string nextLevelName = "RyanMainMenu";
+    Animator winAnim;
+
+    void Start()
+    {
+        winAnim = GameObject.FindGameObjectWithTag("Win").GetComponent<Animator>();
+    }
+
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        PlayerStats playerStats = collider.GetComponent<PlayerStats>();
+
+        if (playerStats != null)
+        {
+            if (checkObjectiveComplete(playerStats))
+            {
+                playerStats.reachedGoal++;
+                winAnim.SetTrigger("WinGame");
+            }
+        }
+
+
+    }
+
+    bool checkObjectiveComplete(PlayerStats playerStats)
+    {
+        if (playerStats.keysCollected < checkKeyCollected)
+        {
+            return false;
+        }
+        if (playerStats.itemsCollected < checkItemsCollected)
+        {
+            return false;
+        }
+        return true; 
+    }
+}
