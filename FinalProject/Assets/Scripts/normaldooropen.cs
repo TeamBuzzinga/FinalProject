7d3c0db3a7c32e54a1da69fe4f6e0ad5aa29fffa
dingfs <dingfengshao@gmail.com>
2015-12-06 15:15:55 -0500

    open meeting room door in level 2

---
+++ FinalProject/Assets/Scripts/normaldooropen.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class normaldooropen : MonoBehaviour {
+    public Transform door1;
+    public Transform door1OpenPosition;
+    private Vector3 door1initial;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            openDoors();
+
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            closeDoors();
+        }
+    }
+
+    void openDoors()
+    {
+        door1initial = door1.position;
+        door1.position = door1OpenPosition.position;
+    }
+
+    void closeDoors()
+    {
+        door1.position = door1initial;
+    }
+}
