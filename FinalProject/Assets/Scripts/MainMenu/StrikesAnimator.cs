42a57c0757236ab6521b96120885da8607cfb1e5
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-07 06:37:01 -0500

    Strikes UI added

---
+++ FinalProject/Assets/Scripts/MainMenu/StrikesAnimator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrikesAnimator : MonoBehaviour {
+    Animator anim;
+    PlayerStats playerStats;
+
+    void Start()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetInteger("Strikes", playerStats.strikes);
+    }
+}
