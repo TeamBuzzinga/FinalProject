e4a8941203d9cd1d0f61bcd1d2f8cbaf5c90200c
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-07 06:58:44 -0500

    Icons added to UI

---
+++ FinalProject/Assets/Scripts/MainMenu/IconHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IconHandler : MonoBehaviour {
+    public Image food;
+    public Image keys;
+    PlayerStats playerStats;
+
+	// Use this for initialization
+	void Start () {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (playerStats.keysCollected > 0)
+        {
+            keys.enabled = true;
+        }
+        if (playerStats.itemsCollected > 0)
+        {
+            food.enabled = true;
+        }
+	}
+}
